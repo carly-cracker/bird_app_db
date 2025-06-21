b1e4b7f1a027b56716260dba6db8f28b91122b72
Carlos Korir <carlkorir226@gmail.com>
2025-06-21 13:05:41 +0300

    fixes the db uri

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_restful import Api, Resource
 from models import db, Bird
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
